1910dbb301dfb1e7ef33a36eac21b3f35bbccbd3
Muhammad Okasha <bsef23a004@pucit.edu.pk>
2026-06-20 22:48:04 +0500

    fix serverless issue

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,11 @@ const connectDB = async () => {
       return;
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
